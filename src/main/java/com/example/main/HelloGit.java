a3d01b7a7b91b3f5720d8d0c2c83fdf94a8d6226
yihangdeng <1512366594@qq.com>
2021-09-06 16:43:03 +0800

    third commit

--- src/main/java/com/example/main/HelloGit.java
+++ src/main/java/com/example/main/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Git01");
+        System.out.println("Git02");
     }
 }
