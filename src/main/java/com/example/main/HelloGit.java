a2872cd82246dd358e99e616a6a413b099a4a07f
yihangdeng <1512366594@qq.com>
2021-09-06 16:38:37 +0800

    first commit

---
+++ src/main/java/com/example/main/HelloGit.java
@@ -0,0 +1,11 @@
+package com.example.main;
+
+/**
+ * @author yhd
+ * @create 2021-09-06 16:35
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Git");
+    }
+}
